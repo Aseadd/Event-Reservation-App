2a920e0b64f46f973263420662749e833aea89e1
addis <betsega23@gmail.com>
2023-02-17 23:33:44 +0300

    Add navbar

---
+++ src/Nav.jsx
@@ -0,0 +1,16 @@
+
+const Nav = () => {
+
+
+    return(
+        <div className='navbar'>
+            <ul>
+                <li>Home</li>
+                <li>Event Booking</li>
+                <li>Event List</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav 
