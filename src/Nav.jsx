fb3c7753b87e1f3e2e1e41134f4235268da4821b
addis <betsega23@gmail.com>
2023-02-22 20:18:59 +0300

    Add navigation link

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 
 const Nav = () => {
-
+const navigate = useNavigate()
 
     return(
         <div className='navbar'>
             <ul>
-                <li>Home</li>
-                <li>Event Booking</li>
-                <li>Event List</li>
+                <li onClick={() => navigate('/')}>Dashboard</li>
+                <li onclick={() => navigate('/events')}>Events</li>
+                <li onclick={() => navigate('/help')}> Help</li>
+                <li onclick= {() => navigate('/logout')}>Logout</li>
             </ul>
         </div>
     )
